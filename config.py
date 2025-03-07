90c47f2fd8300bd262fb535fe9f9ee9e5b7ddcdd
DoublesWang <30306019+DoublesWang@users.noreply.github.com>
2025-03-07 16:24:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,6 +18,8 @@ TELEGRAM_BOT_TOKEN = "" or os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = "" or os.getenv("TELEGRAM_CHAT_ID")
 # wxpusher推送时需填
 WXPUSHER_SPT = "" or os.getenv("WXPUSHER_SPT")
+# Server酱推送时需填
+SERVERCHAN_SCKEY = "" or os.getenv("SERVERCHAN_SCKEY")
 # read接口的bash命令，本地部署时可对应替换headers、cookies
 curl_str = os.getenv('WXREAD_CURL_BASH')
 
